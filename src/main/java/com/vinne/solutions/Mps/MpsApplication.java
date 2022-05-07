bbd0b80ea12e4f148bbcb870f8c9aa44cbe10f20
Emiso <emisondias1@gmail.com>
2022-05-07 15:38:49 -0300

    first

---
+++ src/main/java/com/vinne/solutions/Mps/MpsApplication.java
@@ -0,0 +1,13 @@
+package com.vinne.solutions.Mps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinne/solutions/Mps/MpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinne.solutions.Mps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
